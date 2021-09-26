9a0e4fdf80c442d673f9651dcfe1d8cdbc267410
functrl <cityman177@gmail.com>
2021-09-26 10:53:50 +0300

    add articles

---
+++ server/src/article/article.service.ts
@@ -0,0 +1,73 @@
+import {HttpException, HttpStatus, Injectable} from '@nestjs/common';
+import {UserEntity} from '@app/user/user.entity';
+import {CreateArticleDto} from '@app/article/dto/create-article.dto';
+import {ArticleEntity} from '@app/article/article.entity';
+import {InjectRepository} from '@nestjs/typeorm';
+import {DeleteResult, Repository} from 'typeorm';
+import {ArticleResponseInterface} from '@app/article/types/article-response.interface';
+import slugify from 'slugify';
+
+@Injectable()
+
+export class ArticleService {
+    constructor(@InjectRepository(ArticleEntity) private readonly articleRepositiry: Repository<ArticleEntity>) {
+    }
+    async createArticle(currentUser: UserEntity, createArticleDto: CreateArticleDto): Promise<ArticleEntity> {
+        const article = new ArticleEntity()
+        Object.assign(article, createArticleDto)
+
+        if (!article.tagList) {
+            article.tagList = [];
+        }
+        article.slug = this.getSlug(createArticleDto.title);
+
+        article.author = currentUser;
+
+        return await this.articleRepositiry.save(article);
+    }
+
+    buildArticleResponse(article: ArticleEntity): ArticleResponseInterface {
+        return { article };
+    }
+
+    private getSlug(title: string): string {
+        return (
+            slugify(title, {lower: true}) + '-' +
+            ((Math.random() * Math.pow(36, 6)) | 0).toString(36)
+        );
+    }
+
+    async findBySlug(slug: string): Promise<ArticleEntity> {
+        return await this.articleRepositiry.findOne({slug});
+    }
+
+    async deleteArticle(slug: string, currentUserId: number): Promise<DeleteResult> {
+        const article = await this.findBySlug(slug);
+
+        if (!article) {
+            throw new HttpException('Article does not exist', HttpStatus.NOT_FOUND);
+        }
+
+        if (article.author.id !== currentUserId) {
+            throw new HttpException('You are not an author', HttpStatus.FORBIDDEN);
+        }
+        return await this.articleRepositiry.delete({slug});
+    }
+
+    async updateArticle(slug: string, updateArticleDto: CreateArticleDto,
+                        currentUserId: number): Promise<ArticleEntity> {
+        const article = await this.findBySlug(slug);
+
+        if (!article) {
+            throw new HttpException('Article does not exist', HttpStatus.NOT_FOUND);
+        }
+
+        if (article.author.id !== currentUserId) {
+            throw new HttpException('You are not an author', HttpStatus.FORBIDDEN);
+        }
+
+        Object.assign(article, updateArticleDto);
+
+        return await this.articleRepositiry.save(article);
+    }
+}
